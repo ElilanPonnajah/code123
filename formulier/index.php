fc5b9c76de455966a5ba3a53414aca43d0b1ed8d
ElilanPonnajah <302905573@student.rocmondriaan.nl>
2025-04-16 14:33:24 +0200

    sdasdadasdsadsadasd

--- formulier/index.php
+++ formulier/index.php
@@ -1,39 +1,44 @@
-    <?php ?>
+<?php ?>
 
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
-    <p>Tekstveld</p>
-    <input type="text" value="tekstveld">
-    <p>Password</p>
-    <input type="password" value="Password">
-   
-    <p>Radio input</p>
-    <input type="radio" id="1" name="group1" checked>
-    <input type="radio" id="2" name="group1">
-    <p>Checkbox input</p>
-    <input type="checkbox" name="check1">
-    <input type="checkbox" name="check2" checked>
-    <p>Select (dropdown)</p>
-    <select name="" id="">
-        <option value="" selected>Keuze 1</option>
-        <option value="" >Keuze 2</option>
-        <option value="" >Keuze 3</option>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>Naam</p>
+<input type="text" value="naam">
 
+<p>Email</p>
+<input type="text" value="email">
 
-    </select>
-    <p>Tekst area</p>
-    <textarea name="" id="" cols="30" rows="10">
-        lorem ipsum dolor sit amet, consectetur adipiscing elit
-    </textarea> <br>
+<p>Password</p>
+<input type="password" value="Password">
 
-    <button name="submit">Verzenden </button>
-    </body>
-    </html>
+<p>Radio input</p>
+<input type="radio" id="1" name="group1" checked>
+<input type="radio" id="2" name="group1">
+
+<p>Checkbox input</p>
+<input type="checkbox" name="check1">
+<input type="checkbox" name="check2" checked>
+
+<p>Select (dropdown)</p>
+<select name="" id="">
+    <option value="" selected>Keuze 1</option>
+    <option value="">Keuze 2</option>
+    <option value="">Keuze 3</option>
+</select>
+
+<p>Tekst area</p>
+<textarea name="" id="" cols="30" rows="10">
+    lorem ipsum dolor sit amet, consectetur adipiscing elit
+</textarea> <br>
+
+<button name="submit">Verzenden</button>
+</body>
+</html>
